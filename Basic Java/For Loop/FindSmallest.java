b77870483d6d30e157bf7cb82bce073ef7c8fff8
Sahilpatil2320 <sahilpatil222005@gmail.com>
2026-08-21 21:43:58 +0530

    Find smallest element in an integer array

---
+++ Basic Java/For Loop/FindSmallest.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class FindSmallest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the length of array: ");
+        int n = sc.nextInt();
+
+        if( n <= 0){
+            System.out.println("Array length must be greater than zero.");
+            return;
+        }
+
+        int [] arr = new int[n];
+        System.out.println("Enter elements in array: ");
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int smallest = Integer.MAX_VALUE;
+        for(int i = 0; i< n; i++){
+            if(arr[i] < smallest){
+                smallest = arr[i];
+            }
+        }
+
+        System.out.println("Smallest element is: "+smallest);
+        
+        sc.close();
+    }
+}
