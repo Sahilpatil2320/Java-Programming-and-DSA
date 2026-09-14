b4582aba5a35ee5ee30b4aecb4ff2662ed529a48
Sahilpatil2320 <sahilpatil222005@gmail.com>
2026-09-14 22:01:29 +0530

    solve first repeating element in array

---
+++ Basic Java/For Loop/FirstRepeatingElementInArray.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class FirstRepeatingElementInArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the length of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        boolean found = false;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+                if (arr[i] == arr[j]) {
+                    System.out.println("First repeating element = " + arr[i]);
+                    found = true;
+                    break;
+                }
+            }
+
+            if (found) {
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("No repeating element");
+        }
+
+        sc.close();
+    }
+}
